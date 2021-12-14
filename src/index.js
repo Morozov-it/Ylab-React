a503a2a338044655cacf4ab62a278c4a2933e3b9
Morozov-it <morozov.it2020@gmail.com>
2021-12-14 22:49:36 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Store from './store';
+import App from './app';
+import StoreProvider from "./store/provider";
+import * as modules from './store/exports.js';
+
+const root = document.getElementById("app");
+
+// Состояние приложения
+const store = new Store(modules);
+
+// Сообщаем реакту что и куда рендерить.
+ReactDOM.render(
+  <StoreProvider store={store}>
+    <App/>
+  </StoreProvider>,
+  root
+);
